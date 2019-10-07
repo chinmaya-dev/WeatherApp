dc80a5674feee20029fa97878a82b09c267be20f
Chinmaya Prasad Ojha <chinmaya.dfg@gmail.com>
2019-10-08 00:42:36 +0530

    completed the city validation and error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherServiceService } from './weather-service.service';
 import { IDetails } from './models/weatherdetails.model';
+import { FormGroup,FormControl,FormBuilder,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +9,75 @@ import { IDetails } from './models/weatherdetails.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  form: FormGroup;  
   weatherDetails:IDetails;
-  degreeCelsuis:string;
+  degreeUnit:string;
+  maxDegreeCelsuis:string;
+  minDegreeCelsuis:string;
+  TempUnit:string;
   title = 'cityweatherapp';
+  CityName:string;
+  isCelsius:boolean = false;
+  isKelvin:boolean = false ;
+  isFarh:boolean = false ;
+  errorMessage:string;
+  isDisplayReport:boolean = true;
+  isLoader:boolean;
   constructor(private apiService: WeatherServiceService) { }
-  ngOnInit() {
+  getWeatherByCityName()
+  {
+    this.isLoader = true;
+    this.apiService.getWeatherApiUrl(this.CityName);
     this.apiService.getWeatherDetails().subscribe((data)=>{
       //console.log(data);
       this.weatherDetails = data;
-      console.log(this.weatherDetails.main.temp);
-      this.degreeCelsuis = (this.weatherDetails.main.temp - 273.15).toFixed(2) ;
-      console.log(this.degreeCelsuis);
-    });
+   
+        console.log(this.weatherDetails.main.temp);
+        
+        if(this.TempUnit == "Kelvin")
+        {
+          this.isCelsius = false;
+          this.isFarh = false;
+          this.isKelvin = true;
+          this.degreeUnit = (this.weatherDetails.main.temp).toFixed(2) ;
+          this.maxDegreeCelsuis = (this.weatherDetails.main.temp_max).toFixed(2) ;
+          this.minDegreeCelsuis = (this.weatherDetails.main.temp_min).toFixed(2) ;
+        }
+        else if(this.TempUnit == "Farh")
+        {
+          this.isKelvin = false;
+          this.isCelsius = false;
+          this.isFarh = true;
+          this.degreeUnit = (9/5*(this.weatherDetails.main.temp - 273.15)+32).toFixed(2) ;
+          this.maxDegreeCelsuis = (9/5*(this.weatherDetails.main.temp_max - 273.15)+32).toFixed(2) ;
+          this.minDegreeCelsuis = (9/5*(this.weatherDetails.main.temp_min - 273.15)+32).toFixed(2) ;
+        }
+        else{
+          this.isFarh = false;
+          this.isKelvin = false;
+          this.isCelsius = true;
+          this.degreeUnit = (this.weatherDetails.main.temp - 273.15).toFixed(2) ;
+          this.maxDegreeCelsuis = (this.weatherDetails.main.temp_max - 273.15).toFixed(2) ;
+          this.minDegreeCelsuis = (this.weatherDetails.main.temp_min - 273.15).toFixed(2) ;
+          
+        }
+        this.isLoader=false;
+        this.isDisplayReport = true;
+      
+
+      
+     
+    },
+    (error) => {
+    this.errorMessage = "!Invalid city name ,unable to display weather report";
+ 
+    this.isDisplayReport = false;
+    this.isLoader=false; });
   }
+  ngOnInit() {
+    //this.apiService.getWeatherApiUrl("Delhi");
+    this.getWeatherByCityName();
+   
+  }
+  
 }
--- src/app/weather-service.service.ts
+++ src/app/weather-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 import { IDetails } from './models/weatherdetails.model';
 
 @Injectable({
@@ -10,8 +11,34 @@ export class WeatherServiceService {
 
   constructor(private httpClient: HttpClient) { }
   API_KEY = '3492d471aae7d96ded83eba070532b16';
-  
+  serviceUrl: string;
+  getWeatherApiUrl(CityName: string) {
+    console.log(CityName);
+    if (CityName == undefined){
+      CityName = "Delhi";
+    }
+    //this.serviceUrl = './assets/trip_data.json';
+    //this.serviceUrl = 'http://ardtcew01:8010/api/trip/gettrip?tripnumber=117107100';
+    this.serviceUrl = `https://api.openweathermap.org/data/2.5/weather?q=`+ CityName +`&appid=${this.API_KEY}`;
+    console.log(this.serviceUrl);
+  }
+
   public getWeatherDetails(): Observable<IDetails>{
-    return this.httpClient.get<IDetails>(`https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=${this.API_KEY}`);
+    return this.httpClient.get<IDetails>(this.serviceUrl).pipe(
+      retry(1),
+      catchError(this.handleError)
+    );
+  }
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    //window.alert(errorMessage);
+    return throwError(errorMessage);
   }
 }
